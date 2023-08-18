bbb7045113d3ae789f159947ea3e21f50f8fc26d
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-18 07:36:09 -0500

    feat: :sparkles: App.js
    Se levanta el servidor con su respectiva variable de entorno
    
    by: Jhon Hernandez

---
+++ app.js
@@ -0,0 +1,8 @@
+import dotenv from 'dotenv'
+import express from 'express'
+
+dotenv.config("../")
+let app = express()
+
+let config = JSON.parse(process.env.MY_CONFIG)
+app.listen(config, () => console.log(`http://${config.hostname}:${config.port}`))
