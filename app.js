5f0654cbec347ef04128bca7ceaa710e379eeaac
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-08-18 09:15:47 -0500

    feat: :sparkles: Routes
    Se crea la ruta de la tabla de citas
    
    by: Jhon Hernandez

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import dotenv from 'dotenv'
 import express from 'express'
 
+import storageAcudiente from './routes/acudiente.js';
+import storageCita from './routes/cita.js';
+
 dotenv.config("../")
 let app = express()
+app.use(express.json())
+
+app.use("/acudiente", storageAcudiente)
+app.use("/cita", storageCita)
 
 let config = JSON.parse(process.env.MY_CONFIG)
 app.listen(config, () => console.log(`http://${config.hostname}:${config.port}`))
